f51d3f84d796421bb82489d61c6d9b65af966e8e
Mark O. Vinner <mark.vin.1634@gmail.com>
2014-06-28 08:41:45 +0400

    Initial

---
+++ language.lua
@@ -0,0 +1,104 @@
+--[[
+EDITING NOTES: use structure:
+	ONE TAB PRESS
+	variable_name = "text"
+	-- <- this is a commentary, you can comment by adding "--" before the line you want to comment
+	variable's names are not changable, you MAY NOT change them
+	for each language there is a devinition:
+	--			LANGUAGE_NAME
+	like this:
+	--			ENGLISH
+	--			RUSSIAN
+	etc.
+RUSSIAN LANGUAGE IS ALREADY DONE!]]--
+
+
+--				ENGLISH
+	btitle__close = "Close"
+
+--main menu
+	__title__mainmenu = "Simple Assistant's Menu"
+
+	btitle__cheat_list = "Cheats List"
+	btitle__skills_menu = "Skills Menu"
+	btitle__money_menu = "Money Menu"
+	btitle__disable_anticheat = "Disable Integrated Anticheat"
+	btitle__check_updates = "Check For Updates"
+--cheats menu
+	__title__cheatsmenu = "Cheats Menu"
+
+	btitle__gamerelated_cheats = "Game-Related Cheats"
+	btitle__missionrelated_cheats = "Mission-Related Cheats"
+	btitle__back_to_main_menu = "Back To Main Menu"
+--game-related cheats menu
+	__title__gamerelated_cheats = "Game-Related Cheats"
+
+	btitle__enable_gm = "Enable Godmode"
+	btitle__disable_gm = "Disable Godmode"
+	btitle__enable_iammo = "Enable Infinite Ammo"
+	btitle__enable_instreload = "Enable Instant Reloading"
+	btitle__enable_bdamage = "Buffer Weapon Damage"
+	btitle__disable_fb = "Disable Flashbang Effect"
+	btitle__disable_spread = "Disable Weapon Spread"
+	btitle__disable_recoil = "Disable Recoil"
+	btitle__gagepacks_detector = "Detect Gage Packs"
+	btitle__detect_engine = "Detect Correct Engine"
+	btitle__enable_stacker = "Enable Carrystacker"
+	btitle__back_to_cheats_menu = "Back to Cheats Menu"
+--menu-related cheats
+	__title__missionrelated_cheats = "Menu-Related Cheats"
+
+	btitle__freecontracts = "Free Contracts"
+	btitle__freecasino = "Free Casino Bets"
+	btitle__free_unlimited_preplanning_assets = "Free And Unlimited Preplanning Assets"
+	btitle__unlimited_draws = "Unlimited Preplanning Draws"
+	btitle__finish_mission = "Finish This Mission"
+--gage packs detector
+	__title__gagepacks_menu = "Gagepacks Detector"
+	__title__gagepacks_menu_description = "Press \"Detect All Packages\" button to find 'em"
+
+	btitle__detect_packages = "Detect All Packages"
+--update notifier menu
+	btitle__yes = "Yes"
+	btitle__no = "No"
+	
+	__title__update_avaiable = "Simple Assistant Trainer: Update Available"
+	__title__update_avaiable_description = "Would you like to update your SAT?"
+	__title__update_note_required = "Simple Assistant Trainer: No Update Available"
+	__title__update_note_required_description = "Your SAT is up-to-date, no need to worry!"
+--skillsmenu
+	btitle__add_1_skill = "Add 1 point"
+	btitle__add_5_skill = "Add 5 points"
+	btitle__add_10_skill = "Add 10 points"
+	btitle__add_50_skill = "Add 50 points"
+
+	btitle__rem_1_skill = "Remove 1 point"
+	btitle__rem_5_skill = "Remove 5 points"
+	btitle__rem_10_skill = "Remove 10 points"
+	btitle__rem_50_skill = "Remove 50 points"
+--notices
+	_info_title__iammo_enabled = "Infinite ammo enabled"
+	_info_title__flashbang_disabled = "Flashbang effect disabled"
+	_info_title__spread_disabled = "Spreading disabled"
+	_info_title__recoil_disabled = "Recoil disabled"
+	_info_title__instreload_disabled = "Instant reload enabled"
+	_info_title__godmode_enabled = "Godmode enabled"
+	_info_title__godmode_disabled = "Godmode disabled"
+	_info_title__carrystacker_enabled = "Carry stacking enabled"
+	_info_title__plusdamage_enabled = "Buffered damage"
+	_info_title__host_only = "Only host can execute this"
+	_info_title__packs_shown = "Gage Packs Shown"
+--errors
+	__title__error_unavailable_in_menu = "Unable to execute this cheat"
+	__title__error_unavailable_in_menu_reason = "Please start heist first!"
+	__title__error_packs_not_shown = "Unable to Detect Gage Packs"
+	__title__error_packs_not_shown_reason = "Please detect Gage Packs only when running heist!"
+	__title__error_carrystacker_enabler = "Error: Cannot enable carrystacker"
+	__title__error_carrystacker_enabler_reason = "Please enable carrystacker only when running heist!"
+	__title__error_ac_disabler = "Error: Cannot disable anticheat"
+	__title__error_ac_disabler_reason = "Please disable anticheat only when running heist!"
+	__title__error_only_bigoil_d2 = "Error: This cheat is avaiable only in Big Oil: Day 2"
+--			GERMAN
+--			UKRANIAN
+--			BELORUSSIAN
+--			OTHER (RFTM)
